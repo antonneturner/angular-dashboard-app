e9dd9957986bb3f33d157795043f9be191ebc421
Antonne <antonneturner@gmail.com>
2023-03-13 10:58:15 -0400

    - created wikipedia icon - created google icon - created routes for notes, tabs and todos

--- angular-dashboard/src/app/app-routing.module.ts
+++ angular-dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: 'notes', component: NotesComponent },
+  { path: 'todos', component: TodosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
